2eb466a253f642a72d3a3ff3f10174e604b1ec4f
Burcu Taş <burcutas636@gmail.com>
2023-03-25 17:06:47 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function($) {
+
+	"use strict";
+    
+    // scroll
+    var scrollWindow = function() {
+        $(window).scroll(function(){
+            var $w = $(this),
+                    st = $w.scrollTop(),
+                    navbar = $('.site_navbar'),
+                    sd = $('.js-scroll-wrap');
+
+            if (st > 150) {
+                if ( !navbar.hasClass('scrolled') ) {
+                    navbar.addClass('scrolled');	
+                }
+            } 
+            if (st < 150) {
+                if ( navbar.hasClass('scrolled') ) {
+                    navbar.removeClass('scrolled sleep');
+                }
+            } 
+            if ( st > 350 ) {
+                if ( !navbar.hasClass('awake') ) {
+                    navbar.addClass('awake');	
+                }
+                
+                if(sd.length > 0) {
+                    sd.addClass('sleep');
+                }
+            }
+            if ( st < 350 ) {
+                if ( navbar.hasClass('awake') ) {
+                    navbar.removeClass('awake');
+                    navbar.addClass('sleep');
+                }
+                if(sd.length > 0) {
+                    sd.removeClass('sleep');
+                }
+            }
+        });
+    };
+    scrollWindow();
+
+    
+});
+
+
+
+
+let calcScrollValue = () =>{
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = 
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = 
+    `conic-gradient(#262323 ${scrollValue}%, #aaaaaa ${scrollValue}% )`;
+}
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
